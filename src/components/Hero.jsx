7e5c6cded602a21965a3c20c88d5c6cf59cabfa4
yusra230 <yusra4501434@cloud.neduet.edu.pk>
2024-12-29 20:08:33 +0500

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,39 +1,29 @@
 const Hero = () => {
-  return (
-    <>
-      <section className="bg-bgColor text-white">
-        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-          <div className="mx-auto max-w-3xl text-center">
-            <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
-              I'M A FRONTEND
-              <span className="sm:block"> Increase Conversion. </span>
-            </h1>
-
-            <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-              illo tenetur fuga ducimus numquam ea!
-            </p>
-
-            <div className="mt-8 flex flex-wrap justify-center gap-4">
-              <a
-                className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-                href="#"
-              >
-                Get Started
-              </a>
-
-              <a
-                className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
-                href="#"
-              >
-                Learn More
-              </a>
+    return (
+      <>
+        <section className="bg-bgColor text-white relative">
+          <div className="mx-auto max-w-screen-2xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+            {/* Image positioned absolutely */}
+            <img
+              src="star-1.svg"
+              alt=""
+              className="absolute left-1/3 transform -translate-x-1/2 w-99 h-99"
+            />
+  
+            {/* Text content */}
+            <div className="relative mx-auto max-w-6xl text-center">
+              <h1 className="text-3xl font-extrabold sm:text-9xl text-white">
+                I'M A FRONTEND
+                <span className="sm:block"> DESIGNER </span>
+              </h1>
             </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
-};
 
-export default Hero;
+            {/* <img src="author.png" alt="" /> */}
+          </div>
+        </section>
+      </>
+    );
+  };
+  
+  export default Hero;
+  
