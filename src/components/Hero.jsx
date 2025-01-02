f407a6ac2f5356496e40e1c7f1099c703329db1f
yusra230 <yusra4501434@cloud.neduet.edu.pk>
2025-01-02 18:03:51 +0500

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,34 @@
 const Hero = () => {
-    return (
-      <>
-        <section className="bg-bgColor text-white relative">
-          <div className="mx-auto max-w-screen-2xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-            {/* Image positioned absolutely */}
-            <img
-              src="star-1.svg"
-              alt=""
-              className="absolute left-1/3 transform -translate-x-1/2 w-99 h-99"
-            />
-  
-            {/* Text content */}
-            <div className="relative mx-auto max-w-6xl text-center">
-              <h1 className="text-3xl font-extrabold sm:text-9xl text-white">
-                I'M A FRONTEND
-                <span className="sm:block"> DESIGNER </span>
-              </h1>
-            </div>
+  return (
+    <>
+      <section className="bg-bgColor text-white relative">
+        <div className="mx-auto max-w-screen-2xl px-4 py-32 lg:flex lg:h-screen lg:items-center relative">
+          {/* Star image */}
+          <img
+            src="star-1.svg"
+            alt=""
+            className="absolute left-1/3 transform -translate-x-1/2 w-99 h-99"
+          />
 
-            {/* <img src="author.png" alt="" /> */}
+          {/* Text content */}
+          <div className="relative mx-auto max-w-6xl text-center">
+            {/* Author image positioned above text */}
+            <img
+              src="author.png"
+              alt="Author"
+              className="absolute top-[-250px] left-1/2 transform -translate-x-1/2"
+            />
+
+            {/* Text */}
+            <h1 className="text-3xl font-extrabold sm:text-9xl text-white">
+              I'M A FRONTEND
+              <span className="sm:block"> DEVELOPER </span>
+            </h1>
           </div>
-        </section>
-      </>
-    );
-  };
-  
-  export default Hero;
-  
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
